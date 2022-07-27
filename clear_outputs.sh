8863774522d3f17e050d98a3ef35252a680ba032
mauriciodev <mauricio.dev@gmail.com>
2022-07-27 16:37:16 -0300

    Cleanup and a checker was added.

--- clear_outputs.sh
+++ clear_outputs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace  braz_iono_series.ipynb
+jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace  example_data_usage.ipynb
 echo cp ./MyDrive/0braz/iono_series.csv ./
 cp ./MyDrive/0braz/iono_series.csv ./
